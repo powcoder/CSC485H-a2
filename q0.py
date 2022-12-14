ef480253b60f99fb332d96650bba1af0b84ff5f6
powcoder <powcoder@163.com>
2022-12-14 12:35:27 +0800

    CSC485H a2

---
+++ q0.py
@@ -0,0 +1,70 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+#!/usr/bin/env python3
+# Student name: NAME
+# Student number: NUMBER
+# UTORid: ID
+
+import typing as T
+from string import punctuation
+
+from nltk.corpus import stopwords, wordnet as wn
+from nltk.tokenize import word_tokenize
+
+
+def deepest():
+    """Find and print the synset with the largest maximum depth along with its
+    depth on each of its hyperonym paths.
+
+    Returns:
+        None
+    """
+    raise NotImplementedError
+
+
+def superdefn(s: str) -> T.List[str]:
+    """Get the "superdefinition" of a synset. (Yes, superdefinition is a
+    made-up word. All words are made up...)
+
+    We define the superdefinition of a synset to be the list of word tokens,
+    here as produced by word_tokenize, in the definitions of the synset, its
+    hyperonyms, and its hyponyms.
+
+    Args:
+        s (str): The name of the synset to look up
+
+    Returns:
+        list of str: The list of word tokens in the superdefinition of s
+
+    Examples:
+        >>> superdefn('toughen.v.01')
+        ['make', 'tough', 'or', 'tougher', 'gain', 'strength', 'make', 'fit']
+    """
+    raise NotImplementedError
+
+
+def stop_tokenize(s: str) -> T.List[str]:
+    """Word-tokenize and remove stop words and punctuation-only tokens.
+
+    Args:
+        s (str): String to tokenize
+
+    Returns:
+        list[str]: The non-stopword, non-punctuation tokens in s
+
+    Examples:
+        >>> stop_tokenize('The Dance of Eternity, sir!')
+        ['Dance', 'Eternity', 'sir']
+    """
+    raise NotImplementedError
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
